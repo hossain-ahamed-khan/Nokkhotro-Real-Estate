791cf32d2353e916055be52c678c6235b03c7010
hossain <hossainahamedk@gmail.com>
2024-04-16 12:19:01 +0600

    Completed initial setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAb-P-tXCLfJQ71gMhYiFHzcmLizN5tHyI",
+    authDomain: "real-estate-a2758.firebaseapp.com",
+    projectId: "real-estate-a2758",
+    storageBucket: "real-estate-a2758.appspot.com",
+    messagingSenderId: "400023838725",
+    appId: "1:400023838725:web:86e63c2f89ce1b5c99bc6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,11 @@
+
+
+const home = () => {
+    return (
+        <div>
+            <h1 className="text-3xl">Home page</h1>
+        </div>
+    );
+};
+
+export default home;
