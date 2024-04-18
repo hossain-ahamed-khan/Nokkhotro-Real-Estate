391cf8f0315b4782cfae290bbb2c58edc5c3d76e
hossain <hossainahamedk@gmail.com>
2024-04-18 16:33:17 +0600

    Fixed some ui design

--- src/realEstate/RealEstate.jsx
+++ src/realEstate/RealEstate.jsx
@@ -6,17 +6,19 @@ const RealEstate = ({ estate }) => {
     const { id, estate_title, image, price, status } = estate;
     return (
         <div>
-            <Link to={`/estate/${id}`}>
-                <div className="card w-full image-full">
-                    <figure><img src={image} alt="" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">{estate_title}</h2>
-                        <p className="">{price}</p>
-                        <p className="flex justify-end items-end">{status}</p>
+            <div className="card w-full image-full">
+                <figure><img src={image} alt="" /></figure>
+                <div className="card-body">
+                    <div className="w-full flex">
+                        <h2 className="w-8/12 card-title font-semibold text-2xl">{estate_title}</h2>
+                        <p className="flex justify-end w-4/12 text-[#D93C23] text-xl font-bold">{status}</p>
                     </div>
+                    <p className="font-semibold">{price}</p>
+                    <Link to={`/estate/${id}`}>
+                        <button className="w-32 font-semibold bg-[#053a37fa] px-2 py-3 rounded-2xl">View Property</button>
+                    </Link>
                 </div>
-            </Link>
-
+            </div>
         </div>
     );
 };
